433d133b2932c21bc79cbfda32436f82c8ac90e4
zigen <hrlclb@gmail.com>
2018-07-04 19:37:30 +0900

    allow to store hashed password

--- lib/awesome_chat/accounts/user.ex
+++ lib/awesome_chat/accounts/user.ex
@@ -1,20 +1,30 @@
 defmodule AwesomeChat.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  import Comeonin.Bcrypt
 
 
   schema "users" do
     field :avatar_url, :string
     field :name, :string
     field :password_hash, :string
+    field :password, :string, virtual: true
 
     timestamps()
   end
 
   @doc false
+  def changeset(user, %{"password" => password} = attrs) do
+    IO.inspect attrs
+    user
+    |> cast(attrs, [:name, :avatar_url])
+    |> put_change(:password_hash, hashpwsalt(password))
+    |> validate_required([:name, :password_hash])
+  end
+
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :password_hash, :avatar_url])
-    |> validate_required([:name, :password_hash, :avatar_url])
+    |> cast(attrs, [:name, :avatar_url, :password_hash])
+    |> validate_required([:name, :password_hash])
   end
 end
